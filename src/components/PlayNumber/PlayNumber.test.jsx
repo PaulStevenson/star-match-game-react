665343b5a7323b9b775b9c4140ad1ba6a1595f12
Paul Stevenson <pas00013@gmail.com>
2020-04-07 14:17:43 +0100

    Improved PlayNumber tests to check status and style props

--- src/components/PlayNumber/PlayNumber.test.jsx
+++ src/components/PlayNumber/PlayNumber.test.jsx
@@ -2,27 +2,64 @@ import { render, cleanup, fireEvent } from '@testing-library/react';
 import React from 'react';
 import { PlayNumber } from './PlayNumber';
 
-afterEach(() => {
-  cleanup();
-});
+describe('PlayNumber button', () => {
+  const mockOnClickFn = jest.fn();
 
-it('should render nine buttons', () => {
-  const { getByTestId } = render(<PlayNumber />);
+  const props = {
+    number: 3,
+    status: 'available',
+    onClick: mockOnClickFn,
+  };
 
-  const button = getByTestId('number-button');
+  afterEach(() => {
+    cleanup();
+  });
 
-  expect(button).toBeDefined();
-});
+  it('should render', () => {
+    const { getByTestId } = render(<PlayNumber {...props} />);
 
-it('should call props.onClick when clicked', () => {
-  const mockFn = jest.fn();
+    const button = getByTestId('number-button');
 
-  const { getByTestId } = render(<PlayNumber onClick={mockFn} />);
+    expect(button).toBeDefined();
+    expect(button.textContent).toBe('3');
+    expect(button.style.backgroundColor).toBe('lightgray');
+  });
 
-  const button = getByTestId('number-button');
+  it('should call props.onClick when clicked', () => {
+    const { getByTestId } = render(<PlayNumber {...props} />);
 
-  expect(button).toBeDefined();
+    const button = getByTestId('number-button');
 
-  fireEvent.click(button);
-  expect(mockFn).toBeCalledTimes(1);
+    expect(button).toBeDefined();
+
+    fireEvent.click(button);
+    expect(props.onClick).toBeCalledTimes(1);
+  });
+
+  it('should be coloured lightgreen when the status is Used', () => {
+    props.status = 'used';
+    const { getByTestId } = render(<PlayNumber {...props} />);
+
+    const button = getByTestId('number-button');
+    expect(button).toBeDefined();
+    expect(button.style.backgroundColor).toBe('lightgreen');
+  });
+
+  it('should be coloured lightcoral when the status is Wrong', () => {
+    props.status = 'wrong';
+    const { getByTestId } = render(<PlayNumber {...props} />);
+
+    const button = getByTestId('number-button');
+    expect(button).toBeDefined();
+    expect(button.style.backgroundColor).toBe('lightcoral');
+  });
+
+  it('should be coloured deepskyblue when the status is Candidate', () => {
+    props.status = 'candidate';
+    const { getByTestId } = render(<PlayNumber {...props} />);
+
+    const button = getByTestId('number-button');
+    expect(button).toBeDefined();
+    expect(button.style.backgroundColor).toBe('deepskyblue');
+  });
 });
