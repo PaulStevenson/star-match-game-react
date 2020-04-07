4bc76490db0d4e4ff71f446f522ae9e60e2ce659
Paul Stevenson <pas00013@gmail.com>
2020-04-07 09:35:02 +0100

    Tested StarDisplay

--- src/components/StarDisplay/StarDisplay.jsx
+++ src/components/StarDisplay/StarDisplay.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 // import utils from '../utilities/Maths';
 import utils from '../../utilities/Maths';
-
 import './StarDisplay.scss';
 
 export const StarDisplay = props => (
   <React.Fragment>
     {utils.range(1, props.count).map(starId => (
-      <div key={starId} className='star' />
+      <div key={starId} className='star' data-testid='star' />
     ))}
   </React.Fragment>
 );
---
+++ src/components/StarDisplay/StarDisplay.test.jsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Game } from '../Game';
+
+jest.useFakeTimers();
+
+afterEach(() => {});
+
+it('should render with between one and nine stars', () => {
+  const { getAllByTestId } = render(<Game />);
+
+  const star = getAllByTestId('star');
+
+  expect(star).toBeDefined();
+  expect(star.length).toBeGreaterThan(1);
+  expect(star.length).toBeLessThanOrEqual(9);
+});
