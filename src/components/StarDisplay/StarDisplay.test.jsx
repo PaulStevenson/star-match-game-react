3dc07256400dbc9039991f4d12ddee5646208cae
Paul Stevenson <pas00013@gmail.com>
2020-04-10 09:49:58 +0100

    Removed log from Star Display test

--- src/components/StarDisplay/StarDisplay.test.jsx
+++ src/components/StarDisplay/StarDisplay.test.jsx
@@ -1,11 +1,9 @@
-import { render, prettyDOM } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 import { StarDisplay } from './StarDisplay';
 
 it('should render with between one and nine stars', () => {
   const { getByTestId } = render(<StarDisplay count={1} />);
-
-  console.log(prettyDOM());
 
   const star = getByTestId('star');
 
